229b788a5ccf54e703b237ae290a63dd850e6df3
Codewithabrar <162000914+Codewithabrar@users.noreply.github.com>
2024-04-25 01:55:51 +0500

    currency exchange tool

---
+++ main.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const Currency = {
+    USD: 1, //BASE CURRENCY
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: "Enter From Currency",
+        type: 'list',
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: 'to',
+        message: "Enter From Currency",
+        type: 'list',
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter Your amount",
+        type: "number"
+    }
+]);
+let fromAmount = Currency[userAnswer.from];
+let toAmount = Currency[userAnswer.to];
+let Amount = userAnswer.amount;
+let baseAmount = Amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
